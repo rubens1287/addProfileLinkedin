da9b9125785e7d913b23189c9e1b1134f8380d98
rslo <rubens.lobo@gft.com>
2019-07-15 12:03:25 -0300

    nova versão

--- src/main/java/br/com/front/Main.java
+++ src/main/java/br/com/front/Main.java
@@ -34,7 +34,7 @@ public class Main extends JFrame{
 
         super("Linkedin Add profiles");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100,100,600,400);
+        setBounds(100, 100, 600, 400);
         getContentPane().add(panel);
         setVisible(true);
 
