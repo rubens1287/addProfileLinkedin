ed60dd5f99863b87d948bbff4bd2413dca172b87
rslo <rubens.lobo@gft.com>
2019-07-14 22:01:20 -0300

    primeiro commit

---
+++ src/main/java/br/com/front/Main.java
@@ -0,0 +1,169 @@
+package br.com.front;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+
+public class Main extends JFrame{
+    private JButton button;
+    private JPanel panel;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton button1;
+    private JLabel jResults;
+    public static WebDriver driver;
+
+
+    public Main() {
+
+        super("Linkedin Add profiles");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100,100,600,400);
+        getContentPane().add(panel);
+        setVisible(true);
+
+
+
+        button.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+
+                String decodedPath = null;
+                int perfiladd = 0;
+                int pages;
+
+                jResults.setText("0");
+                //System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\drivers\\chromedriver\\win32\\75.0.3770.140\\chromedriver.exe");
+                WebDriverManager.chromedriver().config().setTargetPath("C:\\Users\\"+System.getProperty("user.name")+"\\AppData\\Local\\Google\\Chrome\\Application");
+                WebDriverManager.chromedriver().setup();
+
+                ChromeOptions options = new ChromeOptions();
+                options.setExperimentalOption("debuggerAddress", "127.0.0.1:9014");
+                driver = new ChromeDriver(options);
+                System.out.println(driver.getTitle());
+
+                if(!textField2.getText().isEmpty()){
+                    searchAnyThing(textField2.getText());
+                    esperar(3);
+                    driver.findElement(By.xpath("//span[text()='Pessoas']")).click();
+                }
+
+
+                Actions action = new Actions(driver);
+
+
+                if(textField1.getText().isEmpty()) {
+                    pages = 1;
+                }else {
+                    pages = Integer.parseInt(textField1.getText());
+                }
+
+                esperar(3);
+
+                for (int l = 0; l < pages ; l++) {
+
+                    List<WebElement> conexoes = endOfPage(driver);
+
+                    esperar(3);
+
+                    for (int j = 0; j < conexoes.size(); j++) {
+                        //Verifications.wait(3);
+                        if(conexoes.size() != 0){
+                            if(conexoes.get(j).getText().contains("Conectar")){
+
+                                action.moveToElement( conexoes.get(j)).perform();
+
+                                if(conexoes.get(j).isEnabled()){
+                                    esperar(3);
+                                    new WebDriverWait(driver,10).until(ExpectedConditions.elementToBeClickable(conexoes.get(j)));
+                                    conexoes.get(j).click();
+                                    esperar(1);
+                                    driver.findElement(By.xpath("//div[contains(@class,'send-invite__actions')]//button[2]")).click();
+                                    perfiladd ++;
+                                    jResults.setText(String.valueOf(perfiladd));
+                                }
+                            }
+                        }
+                    }
+                    esperar(1);
+                    avanca(driver);
+                    action.moveToElement(driver.findElement(By.xpath("//span[text()='Avançar']"))).perform();
+                    esperar(1);
+                    driver.findElement(By.xpath("//span[text()='Avançar']")).click();
+                }
+
+
+                JOptionPane.showMessageDialog(null,"Processamento finalizado!");
+            }
+
+        });
+
+
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ProcessBuilder builder = new ProcessBuilder(
+                        "cmd.exe", "/c", "cd \"C:\\Users\\"+System.getProperty("user.name")+"\\AppData\\Local\\Google\\Chrome\\Application\" && " +
+                        "chrome.exe -remote-debugging-port=9014 --user-data-dir=\"C:\\Users\\"+System.getProperty("user.name")+"\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Default\" ");
+                builder.redirectErrorStream(true);
+                Process p = null;
+                try {
+                    p = builder.start();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+
+            }
+        });
+    }
+
+    public static void main(String[] args){
+        new Main();
+    }
+
+    public void searchAnyThing(String value){
+        driver.findElement(By.xpath("//input[@placeholder='Pesquisar']")).clear();
+        driver.findElement(By.xpath("//input[@placeholder='Pesquisar']")).sendKeys(value);
+        driver.findElement(By.xpath("//input[@placeholder='Pesquisar']")).sendKeys(Keys.ENTER);
+    }
+
+    public void avanca(WebDriver driver){
+        for (int i = 0; i < 4; i++) {
+            driver.findElement(By.xpath("//body")).sendKeys(Keys.PAGE_DOWN);
+        }
+    }
+
+    public List<WebElement> endOfPage(WebDriver driver){
+        List<WebElement> conexoes = null;
+        for (int i = 0; i < 4; i++) {
+            conexoes = driver.findElements(By.xpath("//button[@data-control-name='srp_profile_actions']"));
+            driver.findElement(By.xpath("//body")).sendKeys(Keys.PAGE_DOWN);
+            esperar(1);
+        }
+        return conexoes;
+    }
+
+    public static void esperar(int seconds) {
+        try {
+            Thread.sleep(seconds * 1000L);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+}
